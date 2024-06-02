f60a896f2d3d4519f52207864efa3e41a11c8520
Negiko9999 <tamatamaneghi99@gmeil.com>
2024-06-02 13:53:58 +0900

    説明的なコミットメッセージ

--- my_fave/urls.py
+++ my_fave/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sample/', include('sample.urls')),
     path('sample/login/', auth_views.LoginView.as_view(template_name='sample/login.html'), name='login'),
+    path('', RedirectView.as_view(url='/sample/signup/', permanent=False)),  # ルートURLをsignupにリダイレクト
 ]
